09bf009b544ac0cd356d6515c274c84d0aa4bdcf
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2021-02-09 16:34:35 +0800

    update

--- platforms/cuda/src/CudaCoulKernels.cpp
+++ platforms/cuda/src/CudaCoulKernels.cpp
@@ -6,6 +6,7 @@
 #include "openmm/cuda/CudaForceInfo.h"
 #include "openmm/cuda/CudaParameterSet.h"
 #include "openmm/reference/SimTKOpenMMRealType.h"
+#include "CudaKernelSources.h"
 #include <map>
 #include <iostream>
 #include <set>
